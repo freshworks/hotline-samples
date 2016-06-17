f5b4a8dfa902be6cde6173f31dc704e598eecf54
Rahul Ravindran <rahul@freshdesk.com>
2016-06-17 16:24:26 +0530

    phone gap update user
    implementing update user with form for details input

--- PhonegapSample/www/js/index.js
+++ PhonegapSample/www/js/index.js
@@ -37,7 +37,51 @@ var app = {
         //Allow hotline to take care of push notifications
         //If your app already handles push notification then you don't need the following.
         window.Hotline.registerPushNotification("<ANDROID_SENDER_ID>");
-        
+
+        document.getElementById("update_user_form").onclick = function() {
+
+            var email = document.createElement("INPUT");
+            email.setAttribute("type", "email");
+            email.setAttribute("value", "john@example.com");
+            document.body.appendChild(email);
+
+            var name = document.createElement("INPUT");
+            name.setAttribute("type", "text");
+            name.setAttribute("value", "pablo neruda");
+            document.body.appendChild(name);
+            
+            var countryCode = document.createElement("INPUT");
+            countryCode.setAttribute("type", "number");
+            countryCode.setAttribute("value", "+91");
+            document.body.appendChild(countryCode);
+            
+            var phoneNumber = document.createElement("INPUT");
+            phoneNumber.setAttribute("type", "number");
+            phoneNumber.setAttribute("value", "123456");
+            document.body.appendChild(phoneNumber);
+            
+            var externalId = document.createElement("INPUT");
+            externalId.setAttribute("type", "text");
+            externalId.setAttribute("value", "external id");
+            document.body.appendChild(externalId);
+            
+            var submitUser = document.createElement("INPUT");
+            submitUser.setAttribute("type", "button");
+            submitUser.setAttribute("value", "update user");
+            document.body.appendChild(submitUser);
+            submitUser.onclick = function() {
+                 window.Hotline.updateUser({ 
+                   "name" : name.value, 
+                   "email" : email.value, 
+                   "externalId" : externalId.value, 
+                   "countryCode" : countryCode.value,
+                   "phoneNumber" : phoneNumber.value
+                });
+            };
+            
+        };
+    }
+
         setInterval(function(){
             window.Hotline.unreadCount(function(success,val){
                 if(success && val > 0 ){
